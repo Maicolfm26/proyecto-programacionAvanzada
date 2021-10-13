f6cca76a1a7070fd58f24bd19eee615f2caff1c0
Maicol Florez <maicolflorezlds26@gmail.com>
2021-10-12 22:35:00 -0500

    Se quitaron algunas entidades y se agregaron como ElementCollection

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/repositorios/TelefonoRepo.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.proyecto.repositorios;
-
-import co.edu.uniquindio.proyecto.entidades.Telefono;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TelefonoRepo extends JpaRepository<Telefono, String> {
-}
